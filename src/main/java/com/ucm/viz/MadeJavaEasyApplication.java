ef2a6028e8356b4825b35cf5aba7e9be6e668d11
Akash <akashgoswami70@gmail.com>
2023-12-19 18:26:39 +0530

    first commit

---
+++ src/main/java/com/ucm/viz/MadeJavaEasyApplication.java
@@ -0,0 +1,13 @@
+package com.ucm.viz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadeJavaEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadeJavaEasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucm/viz/MadeJavaEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucm.viz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadeJavaEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
